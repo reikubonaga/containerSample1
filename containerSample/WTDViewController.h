539c8ac4cedc50e4d46b262e4530b69b63aab370
Rei Kubonaga <kubonagarei@gmail.com>
2014-01-27 18:59:11 +0900

    Create single application and gitignore

---
+++ containerSample/WTDViewController.h
@@ -0,0 +1,13 @@
+//
+//  WTDViewController.h
+//  containerSample
+//
+//  Created by rei kubonaga on 2014/01/27.
+//  Copyright (c) 2014年 rei kubonaga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WTDViewController : UIViewController
+
+@end
---
+++ containerSample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  containerSample
+//
+//  Created by rei kubonaga on 2014/01/27.
+//  Copyright (c) 2014年 rei kubonaga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WTDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WTDAppDelegate class]));
+    }
+}
