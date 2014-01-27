56bab31838d95912e334fcae844004e37ff0166a
Rei Kubonaga <kubonagarei@gmail.com>
2014-01-27 19:06:43 +0900

    Create parent table view controller and container view controllers

---
+++ containerSample/WTDContainerViewController.m
@@ -0,0 +1,38 @@
+//
+//  WTDContainerViewController.m
+//  containerSample
+//
+//  Created by rei kubonaga on 2014/01/27.
+//  Copyright (c) 2014年 rei kubonaga. All rights reserved.
+//
+
+#import "WTDContainerViewController.h"
+
+@interface WTDContainerViewController ()
+
+@end
+
+@implementation WTDContainerViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
